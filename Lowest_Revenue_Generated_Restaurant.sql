0746e5f26875096f10b838261a63968422d2be57
Joseph Salu <me.jabsal@gmail.com>
2022-01-25 05:25:20 +0100

    Update Lowest_Revenue_Generated_Restaurant.sql

--- Lowest_Revenue_Generated_Restaurant.sql
+++ Lowest_Revenue_Generated_Restaurant.sql
@@ -4,7 +4,6 @@ https://platform.stratascratch.com/coding-question?id=2036
 -- Write a query that returns a list of the bottom 2% revenue generating restaurants. Return a list of restaurant IDs and their total revenue from when customers placed orders in May 2020.
 -- You can calculate the total revenue by summing the order_total column. And you should calculate the bottom 2% by partitioning the total revenue into evenly distributed buckets.
 
-
 -- Solution
 -- Filter data to use only May 2020 using customer_placed_order_datetimme column
 -- Calculate revenue by sum(order_total) and grouping by restaurant_id column
