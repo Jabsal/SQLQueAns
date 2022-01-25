951b1030540d80b1bd1624ebc6b92f21a4df0bc0
Joseph Salu <me.jabsal@gmail.com>
2022-01-25 07:40:41 +0100

    Update Lowest_Revenue_Generated_Restaurant.sql

--- Lowest_Revenue_Generated_Restaurant.sql
+++ Lowest_Revenue_Generated_Restaurant.sql
@@ -1,6 +1,5 @@
-https://platform.stratascratch.com/coding-question?id=2036
-
--- Lowest Revenue Generated Restaurants Question
+-- Lowest Revenue Generated Restaurants Question 
+-- Source https://platform.stratascratch.com/coding-question?id=2036
 -- Write a query that returns a list of the bottom 2% revenue generating restaurants. Return a list of restaurant IDs and their total revenue from when customers placed orders in May 2020.
 -- You can calculate the total revenue by summing the order_total column. And you should calculate the bottom 2% by partitioning the total revenue into evenly distributed buckets.
 
