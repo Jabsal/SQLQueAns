dd9860a7f474a13ca36e0a0e9df22a21d7be9c0e
Joseph Aduratola Salu <UI750530@rwe.com>
2022-01-26 03:07:32 +0100

    implement solution

--- Number_of_violations.sql
+++ Number_of_violations.sql
@@ -9,3 +9,11 @@ https://platform.stratascratch.com/coding/9728-inspections-that-resulted-in-viol
 -- Column needed: business_name, violation_id, inspection_date
 -- year from business_name, count() violation_id for n_inspections
 -- filter for roxanne cafe and inspections where violation_id is not null
+
+SELECT EXTRACT(year from inspection_date::date) as year,
+        COUNT(violation_id) as n_inspections
+FROM sf_restaurant_health_violations
+WHERE business_name = 'Roxanne Cafe'
+AND   violation_id IS NOT NULL
+GROUP BY year
+ORDER BY year asc
