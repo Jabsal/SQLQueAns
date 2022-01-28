8a6df24d4f310dd0827fd0307d27f4e637cf54c1
Joseph Aduratola Salu <UI750530@rwe.com>
2022-01-28 08:17:11 +0100

    analyze solution

---
+++ Marketing_Campaign_Success.sql
@@ -0,0 +1,6 @@
+https://platform.stratascratch.com/coding/514-marketing-campaign-success-advanced
+
+-- Marketing Campaign Success [Advanced]
+-- You have a table of in-app purchases by user. Users that make their first in-app purchase are placed in a marketing campaign where they see call-to-actions for more in-app purchases. Find the number of users that made additional in-app purchases due to the success of the marketing campaign.
+
+--Their first
