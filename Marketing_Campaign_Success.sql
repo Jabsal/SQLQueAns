f761705117eea6126fa2250ed8fcf263c751bbfe
Joseph Aduratola Salu <UI750530@rwe.com>
2022-01-30 07:50:15 +0100

    implement solution

--- Marketing_Campaign_Success.sql
+++ Marketing_Campaign_Success.sql
@@ -3,4 +3,21 @@ https://platform.stratascratch.com/coding/514-marketing-campaign-success-advance
 -- Marketing Campaign Success [Advanced]
 -- You have a table of in-app purchases by user. Users that make their first in-app purchase are placed in a marketing campaign where they see call-to-actions for more in-app purchases. Find the number of users that made additional in-app purchases due to the success of the marketing campaign.
 
---Their first
+--Their first purchase date
+-- Product needs to be purchased on a different day
+-- Product needs to be different
+
+WITH ranked_purchases as 
+(
+SELECT user_id, 
+       product_id, 
+       created_at,
+        dense_rank() over (partition by user_id order by created_at) as rank_1,
+        dense_rank() over (partition by user_id , product_id order by created_at) as rank_2
+FROM marketing_campaign
+)
+
+SELECT COUNT(distinct user_id)
+FROM ranked_purchases 
+WHERE rank_1 > 1 
+      AND rank_2 = 1
