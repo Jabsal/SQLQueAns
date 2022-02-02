09531cc7714cb0410157e0d66f0256555b5233b4
Joseph Aduratola Salu <UI750530@rwe.com>
2022-02-02 09:27:43 +0100

    Highest customer order

--- HighestCostOrder.sql
+++ HighestCostOrder.sql
@@ -1,12 +1,16 @@
 -- https://platform.stratascratch.com/coding/9915-highest-cost-orders
 -- Highest Cost Orders
--- Find the customer with the highest daily total order cost between 2019-02-01 to 2019-05-01. If customer had more than one order on a certain day, sum the order costs on daily basis. Output their first name, total cost of their items, and the date.
+-- Find the customer with the highest daily total order cost between 2019-02-01 to 2019-05-01. 
+-- If customer had more than one order on a certain day, sum the order costs on daily basis. 
+-- Output their first name, total cost of their items, and the date.
  
 --  For simplicity, you can assume that every first name in the dataset is unique.
 
 -- Solution
 -- Join customer and order tables
 -- Filter for order dates between '2019-02-01' AND '2019-05-01'
+-- Group first_name, cust_id, order_date
+-- Order by desc limit 1
 
 SELECT
     c.first_name,
